c41f935b4cb4b209019fdac9be2d3f4fba3aea71
Bhaskar Bahukhandi <95625149+Bhaskar-Bahukhandi@users.noreply.github.com>
2026-09-23 19:26:33 +0530

    Mobile: remove redundant SQLite test import

--- apps/mobile/test/offline/offline_database_upgrade_test.dart
+++ apps/mobile/test/offline/offline_database_upgrade_test.dart
@@ -2,7 +2,6 @@ import "dart:io";
 
 import "package:flutter_test/flutter_test.dart";
 import "package:path/path.dart" as p;
-import "package:sqflite_common/sqlite_api.dart" show OpenDatabaseOptions;
 import "package:sqflite_common_ffi/sqflite_ffi.dart";
 
 import "package:codeflux_mobile/offline/persistence/offline_database.dart";
